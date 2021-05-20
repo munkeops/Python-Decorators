939f3621969af716891712c291fb4da3fc889824
Rohan Russel Nedungadi <nrogun@gmail.com>
2021-05-20 12:41:50 +0530

    poslishing

--- timer.py
+++ timer.py
@@ -1,18 +1,20 @@
 
 import time
 
+
 def timer(func):
 
-    def wrapper(*args,**kwargs):
-        start=time.time()
-        rv=func()
-        end=time.time()
-        total=end-start
-        print("\n\n\nTime To Run : ",total)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        rv = func()
+        end = time.time()
+        total = end-start
+        print("\n\n\nTime To Run : ", total)
         return rv
 
     return wrapper
-        
+
+
 @timer
 def test():
     time.sleep(1.5)
